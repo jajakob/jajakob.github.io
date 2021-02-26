12b3bcf87c509bada5f26b4be4c63c3e7951b2e0
jakejacob <j.jacob.cano@gmail.com>
2021-02-26 08:57:06 +0100

    renamed li and a elements, added projects functionality: nav and slideshow

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const doc_style = getComputedStyle(document.documentElement);
 
 // Get all the list elements
-let li_collection = document.getElementsByTagName("LI");
-const home = li_collection[1].getElementsByTagName("A")[0];
+let li_collection = document.getElementsByClassName("nav-li");
+const home = li_collection[1].getElementsByClassName("nav-li-a")[0];
 
 console.log(li_collection[0]);
 console.log(getComputedStyle(home).height);
@@ -13,17 +13,17 @@ console.log(getComputedStyle(home).height);
 for (let i = 0; i < li_collection.length; i++) {
     li_collection[i].addEventListener("click", function() {
         let current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace("active", "");
-        this.className = "active";
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
     });
 }
 
 const logo = document.getElementById("logo");
 
 logo.addEventListener("click", () => {
-    li_collection[0].className = "active";
+    li_collection[0].className = "nav-li active";
     for (let i = 1; i < li_collection.length; i++) {
-        li_collection[i].className = "";
+        li_collection[i].className = "nav-li";
     }
 
 })
